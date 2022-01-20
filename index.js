c6615c7b4315e6fb6042d983f84aa731db00809c
NOOS3NCE <83935244+NOOS3NCE@users.noreply.github.com>
2022-01-19 22:07:19 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,93 @@
+const express = require('express')
+const app = express()
+const port = 3001
+
+const mantis_model = require('./mantis_model')
+
+app.use(express.json())
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS,PATCH');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+    next();
+});
+
+app.get('/kit', (req, res) => {
+    mantis_model.getKits()
+        .then(response => {
+            res.status(200).send(response);
+        })
+        .catch(error => {
+            res.status(500).send(error);
+        })
+})
+
+app.get('/kit/:id', (req, res) => {
+    mantis_model.getKit(req.params.id)
+        .then(response => {
+            res.status(200).send(response);
+        })
+        .catch(error => {
+            res.status(500).send(error);
+            console.log("REQUEST",req.params.id)
+        })
+})
+app.get('/lens', (req, res) => {
+    mantis_model.getLenses()
+        .then(response => {
+            res.status(200).send(response);
+        })
+        .catch(error => {
+            res.status(500).send(error);
+        })
+})
+app.patch('/lens/swap', (req,res)=>{
+    mantis_model.swapLenses(req)
+        .then(response =>  {
+            res.status(200).send(response)
+        })
+        .catch(error=>{
+            res.status(500).send(error)
+        })
+})
+app.post('/kit', (req, res) => {
+    mantis_model.createKit(req.body)
+        .then(response => {
+            res.status(200).send(response);
+        })
+        .catch(error => {
+            res.status(500).send(error);
+        })
+})
+app.post('/lens', (req, res) => {
+    mantis_model.createLens(req.body)
+        .then(response => {
+            res.status(200).send(response);
+        })
+        .catch(error => {
+            res.status(500).send(error);
+        })
+})
+
+app.delete('/kit/:id', (req, res) => {
+    mantis_model.deleteKit(req.params.id)
+        .then(response => {
+            res.status(200).send(response);
+        })
+        .catch(error => {
+            res.status(500).send(error);
+        })
+})
+
+app.get('/cities',(req,res) => {
+    mantis_model.getCities()
+        .then(response => {
+            res.status(200).send(response);
+        })
+        .catch(error =>{
+            res.status(500).send(error)
+        })
+})
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+})
---
+++ mantis_model.js
@@ -0,0 +1,135 @@
+const {request} = require("express");
+const Pool = require('pg').Pool
+const pool = new Pool({
+    user: 'my_user',
+    host: 'localhost',
+    database: 'postgres',
+    password: 'my_user',
+    port: 5432,
+});
+
+const getKits= () => {
+    return new Promise(function(resolve, reject) {
+        pool.query('SELECT kits.*, cities.city_code, users.user_firstname, users.user_lastname FROM kits LEFT JOIN cities ON kits.city_id = cities.city_id LEFT JOIN users ON kits.user_id = users.user_id GROUP BY kits.kit_id, cities.city_id, users.user_id ORDER BY kits.kit_display ASC', async (error, results) => {
+            if (error) {
+                reject(error)
+            }
+            resolve(results.rows);
+        })
+    })
+}
+const getLenses = () => {
+    return new Promise( function(resolve,reject) {
+        pool.query('SELECT lenses.*, kits.kit_display FROM kits RIGHT JOIN lenses ON lenses.kit_id = kits.kit_id GROUP BY lenses.kit_id, lenses.lens_model, lenses.lens_brand, lenses.lens_serial, lenses.lens_name, lenses.lens_display, lenses.lens_id, kits.kit_id, kits.kit_display ORDER BY lenses.lens_display ASC', async (error, results) => {
+            console.log("Lenses", results.rows)
+            if(error){
+                reject(error)
+            }
+            resolve(results.rows)
+        })
+    })
+}
+const swapLenses=(body)=>{
+    const {lens_id, kit_id} = body.body
+    console.log("BODY:",body.body)
+    console.log("LENS ID: ", lens_id)
+    console.log("KIT ID: ", kit_id)
+    return new Promise(function(resolve,reject){
+        pool.query('UPDATE lenses SET kit_id = $2 WHERE lens_id = $1', [lens_id,kit_id],async(error,results)=>{
+            if(error){
+                reject(error)
+            }
+            resolve(`Lens has been moved to new kit:${results} `)
+        })
+    })
+}
+const getKit = (id) => {
+    return new Promise(function(resolve, reject) {
+        let kit
+        pool.query('SELECT kits.*, cities.city_code FROM kits JOIN cities ON kits.city_id = cities.city_id WHERE kit_id = $1', [id],async (error, results) => {
+            if(error){
+                reject(error)
+            }
+            kit = results.rows[0]
+            pool.query('SELECT * FROM lenses WHERE kit_id = $1', [kit.kit_id],(err, lenses) => {
+                if(error){
+                    reject(error)
+                }
+                pool.query('SELECT * FROM users WHERE user_id = $1', [kit.user_id],(err, users) => {
+                    if(error){
+                        reject(error)
+                    }
+                    const user = users.rows[0]
+                    const lensList = lenses.rows
+                    kit = results.rows[0]
+                    kit.lenses = lensList
+                    kit.user = user
+                    resolve(kit)
+                })
+            })
+        })
+    })
+
+}
+const createKit = (body) => {
+
+    return new Promise(function(resolve, reject) {
+        const {kit_display, city_id, kit_name, kit_type, camera, lenses} = body
+        console.log("KIT BODY: ", body)
+        pool.query('INSERT INTO kits (kit_name, kit_display, kit_type, city_id) VALUES ($3,$1,$4,$2)', [kit_display, city_id, kit_name, kit_type] , (error, results) => {
+            if (error) {
+                reject(error)
+            }
+            resolve(`A new kit has been added: ${results}`)
+        })
+    })
+}
+const createLens = (body) => {
+    return new Promise(function(resolve, reject){
+        const {lens_brand, lens_name, lens_serial, lens_model, lens_display, kit_name} = body
+        console.log("LENS BODY:", body)
+        pool.query('INSERT INTO lenses (lens_brand, lens_name, lens_serial, lens_model, lens_display, kit_id) SELECT $1, $2, $3, $4, $5, kits.kit_id FROM kits WHERE kits.kit_name = $6', [lens_brand, lens_name, lens_serial, lens_model, lens_display, kit_name], (error, results) => {
+            if(error){
+                reject(error)
+            }
+            resolve(`A new lens has been added: ${results}`)
+        })
+    })
+}
+const loadOutKit = (id) => {
+    return new Promise(function(resolve, reject){
+        pool.query('UPDATE kit_loadedOut FROM kits WHERE id = $1', [id])
+    })
+}
+const deleteKit = (id) => {
+    return new Promise(function(resolve, reject) {
+        pool.query('DELETE FROM kits WHERE kit_id = $1', [id], (error, results) => {
+            if (error) {
+                reject(error)
+            }
+            resolve(`Merchant deleted with ID: ${id}`)
+        })
+    })
+}
+const getCities = () => {
+    return new Promise(function(resolve, reject){
+        pool.query('SELECT * FROM cities', (error,results)=>{
+            if(error){
+                reject(error)
+            }
+            resolve(results.rows)
+        })
+    })
+}
+
+module.exports = {
+    getKits,
+    getKit,
+    createKit,
+    deleteKit,
+    getCities,
+    loadOutKit,
+    createLens,
+    getLenses,
+    swapLenses
+}
