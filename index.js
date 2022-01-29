d86ae1395f813c1b7d87fdd94b49158aa1397c19
NOOS3NCE <83935244+NOOS3NCE@users.noreply.github.com>
2022-01-28 21:05:19 -0600

    updated

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const mantis_model = require('./mantis_model')
 
 app.use(express.json())
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', process.env.DB_ORIGIN || 'https://wildorchid.one');
+    res.setHeader('Access-Control-Allow-Origin', 'https://wildorchid.one');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS,PATCH');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
     next();
